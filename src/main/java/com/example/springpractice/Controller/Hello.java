a7d7184f48424fcb3e0a6081c8259f4ffade5c66
zhanghan <1599252137@qq.com>
2022-01-31 14:38:21 +0800

    spring 项目初始化

---
+++ src/main/java/com/example/springpractice/Controller/Hello.java
@@ -0,0 +1,13 @@
+package com.example.springpractice.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    @RequestMapping("/hello")
+    public String getHello (){
+        return "Hello world";
+    }
+}
